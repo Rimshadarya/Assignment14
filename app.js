8c67d44a95be1f7722ae15c1366801e2682e66a6
Rimshadarya <rimshadarya@gmail.com>
2024-09-12 16:19:43 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,675 @@
+// Chapter9
+
+// Question1
+
+// var num = ("Result: <br> The value of a is :10 <br> .............................................. <br> <br> <br> ") 
+
+// document.write(num)
+
+// var a = 10
+
+// document.write("The value of ++a is:")  
+
+// document.write(++a)
+
+// document.write("<br> now the value of a is: ")
+
+// var a = 10
+
+// document.write(++a)
+
+// document.write("<br> <br> <br> The value of a++ is: ")
+
+// var a = 11
+
+// document.write(a++)
+
+// document.write("<br> now the value of a is: ")
+
+// var a = 11
+
+// document.write(++a)
+
+// document.write("<br> <br> <br> The value of --a is: ")
+
+// var a = 12
+
+// document.write(--a)
+
+// document.write("<br> now the value of a is: ")
+
+// var a = 12
+
+// document.write(--a)
+
+// document.write("<br> <br> <br> The value of a-- is: ")
+
+// var a = 12
+
+// document.write(--a)
+
+// document.write("<br> now the value of a is: ")
+
+// var a = 11
+
+// document.write(--a)
+
+// Question2
+
+// var a = 2;var b = 1 
+
+// document.write(--a )
+
+// document.write("<br>")
+
+// var a = 2;var b = 1
+
+// document.write(--a - --b)
+
+// document.write("<br>")
+
+// var a = 2;var b = 1
+
+// document.write(--a - --b + ++b)
+
+// document.write("<br>")
+
+// var a = 2;var b = 1
+
+// document.write(--a - --b + ++b + b--)
+
+// Question3
+
+// var userinput = prompt("Enter your name")
+
+// var greet = prompt("Hello user")
+
+// document.write(userinput + greet)
+
+// Question4
+
+// var num = prompt("Enter Number", "5") //prompt user to enter the number
+
+// var num = parseInt(num); //parse the num to number
+
+// var i = 0;
+
+// document.write('<table border="1" cellspacing="0">');
+
+// for (i = 1; i < 10; i++) {
+
+//   document.write("<tr><td>" + num + " x " + i + " = " + num * i + "</td></tr>");}
+
+// // Question5
+
+// var subjectName = "<b> Subject" ;var totalmarks = "<b> Total Marks"; var obtainedMarks = "<b> Obtained Marks"; var percentage = "Percentage <br>"
+
+// document.write(subjectName + " " + totalmarks + " " + obtainedMarks + " " + percentage)
+
+// var english = "<br> English";var num1 = 100 ;var num2 = 85; var num3 =" 85%"
+
+// document.write(english+" "+ num1 + " " + num2 + " " + num3)
+
+// var urdu = "<br> <br> Urdu";var num4 = 100 ;var num5 = 74; var num6 = "74%"
+
+// document.write(urdu+" "+ num4 + " " + num5 + " " + num6)
+
+// var math = "<br> <br> Math";var num7 = 100 ;var num8 = 74; var num9 = "74%"
+
+// document.write(math+" "+ num7 + " " + num8 + " " + num9)
+
+// Chapter Math Expression
+
+// Question1
+
+// var number1 = 3
+
+// var number2 = 5
+
+// var sum = number1 + number2
+
+// document..write("The sum of "+ number1 + " and " + number2 + " is " + sum)
+
+// Question2
+
+// var number1 = 3
+
+// var number2 = 5
+
+// var sum = number1 - number2
+
+// document.write("The sum of "+ number1 + " and " + number2 + " is " + sum)
+
+// var number1 = 3
+
+// var number2 = 5
+
+// var sum = number1 * number2
+
+// document.write("The sum of "+ number1 + " and " + number2 + " is " + sum)
+
+// var number1 = 3
+
+// var number2 = 5
+
+// var sum = number1 / number2
+
+// document.write("The sum of "+ number1 + " and " + number2 + " is " + sum)
+
+// Question3
+
+// var text = "Value after variable declaration is "
+
+// document.write(text)
+
+// var value = 
+
+// document.write(value)
+
+// var initial = "<br> Initial value:"
+
+// document.write(initial)
+
+// var val = 5
+
+// document.write(val)
+
+// var incre = "<br>Value after increment is:"
+
+// document.write(incre)
+
+// var increment = 5
+
+// document.write(++increment)
+
+// var doc = "<br> Value after addition is: "
+
+// document.write(doc)
+
+// var docum = 7
+
+// document.write(docum + increment)
+
+// var decre = "<br> Value after decrement is:"
+
+// document.write(decre)
+
+// var decrement = 13
+
+// document.write(--decrement)
+
+// var remind = "<br> The reminder is"
+
+// document.write(remind)
+
+// var reminder = 12
+
+// document.write( 0/ reminder)
+
+// Question4
+
+// var oneMovieTicket = 600
+
+// var buying_person = 5
+
+// var price = "Total cost to buy5 tickets to a movie is"
+
+// document.write(price )
+
+// var total_cost =(oneMovieTicket * buying_person)
+
+// document.write(total_cost)
+
+// Question5
+
+// var head = "<b> Table of 4"
+
+// document.write(head)
+
+// var num1 = 4
+
+// var num2 = 1
+
+// var result = ("<br> 4 * 1 = ")
+
+// document.write(result + num1 * num2)
+
+// var num3 = 4
+
+// var num4 = 2
+
+// var result = ("<br> 4 * 2 = ")
+
+// document.write(result + num3 * num4)
+
+// var num5 = 4
+
+// var num6 = 3
+
+// var result = ("<br> 4 * 3 = ")
+
+// document.write(result + num5 * num6)
+
+// var num7 = 4
+
+// var num8 = 4
+
+// var result = ("<br> 4 * 4 = ")
+
+// document.write(result + num7 * num8)
+
+// var num9 = 4
+
+// var num10 = 5
+
+// var result = ("<br> 4 * 5 = ")
+
+// document.write(result + num9 * num10)
+
+// var num11 = 4
+
+// var num12 = 6
+
+// var result = ("<br> 4 * 6 = ")
+
+// document.write(result + num11 * num12)
+
+// var num13 = 4
+
+// var num14 = 7
+
+// var result = ("<br> 4 * 7 = ")
+
+// document.write(result + num13 * num14)
+
+// var num15 = 4
+
+// var num16 = 8
+
+// var result = ("<br> 4 * 8 = ")
+
+// document.write(result + num15 * num16)
+
+// var num17 = 4
+
+// var num18 = 9
+
+// var result = ("<br> 4 * 9 = ")
+
+// document.write(result + num17 * num18)
+
+// var num19 = 4
+
+// var num20 = 10
+
+// var result = ("<br> 4 * 10 = ")
+
+// document.write(result + num19 * num20)
+
+// Question6
+
+// var celcius = 25
+
+// var fahrenhiet = 70
+
+// var num1 = 9
+
+// var num2 = 5
+
+// var num3 = 32
+
+// document.write("25°C is ")
+
+// document.write((celcius*num1/num2)+num3)
+
+// document.write("°F <br>")
+
+// document.write("70°F is ")
+
+// document.write((fahrenhiet-num3)*5/9)
+
+// document.write("°C")
+
+// Question7
+
+// var price_of_item1 = "price of item1 is 650 <br>"
+
+// document.write(price_of_item1)
+
+// var Quantity_of_item1 = "Quantity of item1 is 3 <br>"
+
+// document.write(Quantity_of_item1)
+
+// var price_of_item2 = "Price of item2 is 100 <br>"
+
+// document.write(price_of_item2)
+
+// var Quantity_of_item2 = "Quantity of item2 is 7"
+
+// document.write(Quantity_of_item2)
+
+// var shipping_charges = "<br> Shipping charges 100 <br>"
+
+// document.write(shipping_charges)
+
+// var total_cost_of_your_order = "Total cost of your order is 2750"
+
+// document.write(total_cost_of_your_order)
+
+// Question8
+
+// var heading = "<b> Marks Sheet <br> <br> <br>"
+
+// document.write(heading)
+
+// var total_marks = ("Total marks: 980 <br>")
+
+// var Obtained_marks = ("Obtained marks: 840 <br>")
+
+// var percentage = ("Percentage: 82.0422%")
+
+// document.write(total_marks+Obtained_marks+percentage)
+
+// Question9
+
+// var head = "<b> Currency in PKR <br> <br> <br>"
+
+// document.write(head)
+
+// var USDollar = 104.80 
+
+// var SaudiRiyal = 28
+
+// var total = (USDollar*SaudiRiyal)
+
+// document.write("Total currency in Pkr:")
+
+// document.write(total)
+
+// Question10
+
+// var num1 = 8
+
+// var num2 = 5
+
+// var num3 = 10
+
+// var num4 = 2
+
+// document.write("The answer is ")
+
+// document.write(num1+num2*num3/num4)
+
+// Question10
+
+// var Current_year = 2024
+
+// var Birth_year = 2006
+
+// var your_age = (Current_year-Birth_year)
+
+// document.write("My Cuurent age is ")
+
+// document.write(your_age)
+
+// Question11
+
+// document.write("Radius of a circle is")
+
+// var radius = 20
+
+// document.write(radius)
+
+// document.write("<br> Circumference of a circle is ")
+
+// var Circumference_of_a_circle = 2
+
+// document.write(Circumference_of_a_circle)
+
+// document.write("𝜋r")
+
+// document.write("<br> The area is ")
+
+// var total = (20*2*3.14)
+
+// document.write(total)
+
+// Question12
+
+// var favourite_snack = ("Lays")
+
+// document.write("Favourite snacks is ")
+
+// document.write(favourite_snack)
+
+// document.write("<br>current age is ")
+
+// var current_age = 18
+
+// document.write(current_age)
+
+// document.write("<br>maximum age is ")
+
+// var maximum_age = 65
+
+// document.write(maximum_age)
+
+// document.write("<br>amount of snacks per day")
+
+// var amount_of_snacks_per_day = 2
+
+// document.write(amount_of_snacks_per_day)
+
+// document.write("<br>You will need ") 
+
+// var total = (maximum_age*amount_of_snacks_per_day)
+
+// document.write(total)
+
+// document.write(" lays to last you untill the ripe old age of 65" )
+
+// Chapter6
+
+// Question1
+
+
+
+// Question2
+
+// var x =100
+
+// var y = x
+
+// document.write(--y)
+
+// Question3
+
+// var x =50
+
+// var y = x
+
+// document.write(x++)
+
+// Question4
+
+// var y = 50
+
+// var z = y
+
+// document.write(--y)
+
+// Question5
+
+// var original_number = 56
+
+// var new_number = original_number
+
+// document.write(--new_number)
+
+// Question6
+
+// var original_number = 47
+
+// var new_number = original_number
+
+// document.write(++original_number)
+
+// Quetsion7
+
+// var number_value = 32
+
+// document.write(++number_value)
+
+// alert(number_value)
+
+// Chapter7
+
+// Question1
+
+// var x = 2
+
+// var y = 6
+
+// var calculatedNum = (2+(2*6))
+
+// document.write(calculatedNum)
+
+// Question2
+
+// var x = 2
+
+// var y = 6
+
+// var calculatedNum = ((2+2)*6)
+
+// document.write(calculatedNum)
+
+// Question3
+
+// var x = 2
+
+// var y = 4
+
+// var calculatedNum = ((2+2)*(4+2))
+
+// document.write(calculatedNum)
+
+// Question4
+
+// var x = 2
+
+// var y = 4
+
+// var calculatedNum = (((2+2)*4)+2)
+
+// document.write(calculatedNum)
+
+// Question5
+
+// var a = 2
+
+// var b = 4
+
+// var cost =((2+2)*(4+10))
+
+// document.write(cost)
+
+// var cost1 = (2+(2*4)+10)
+
+// document.write(cost1)
+
+// var cost2 = (4/2*2)
+
+// document.write(cost2)
+
+// chapter8
+
+// Question1
+
+// var num = "2"
+
+// var num1 = "2"
+
+// document.write(num+num1)
+
+// Question2
+
+// var msg = "Hello"
+
+// var msg2 = "Dolly"
+
+// var message = msg +" " +  msg2
+
+// alert(message)
+
+// Question3
+
+// var num = "33"
+
+// var num1 = 3
+
+// alert(num + num1)
+
+// Question4
+
+// var data = "Pakistan"
+
+// var type = "Zindabad"
+
+// alert(data + " " + type)
+
+// document.write(data +" " + type)
+
+// Question4
+
+// var type = "group"
+
+// var type2 = "study"
+
+// document.write(type+" "+type2)
+
+// Question5
+
+// var vari1 = "Seventy"
+
+// var vari2 = "Nine"
+
+// var result = vari1+ " " +vari2
+
+// document.write(result)
+
+// Chapter9
+
+// Question1
+
+// var message = prompt("Enter you First Name")
+
+// Question2
+
+// var country_name = prompt("Enter you country name","China")
+
+// Question3
+
+// var correct = prompt("Enter your name")
+
+// Question4
+
+// var code = prompt(" ","a")
+
+// Question5
+
+// var msg = prompt(" "+ "Hello user")
+
+// var msg2 = prompt("" + "How are you?")
+
+// var user =prompt(" User's response")
+
+// document.write(msg + msg2 + user)
+
+// Question6
+
+var disc = prompt("Thanks for visiting our page")
+
+var space = prompt("" + "uhkkkkkkkkkkkkk")
+
+alert ("This is Rimsha")
